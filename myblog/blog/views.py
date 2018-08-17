df0f65ef11e4ccae0f5502639f49a56e04fe431a
CYR <aiden_cao@outlook.com>
2018-08-17 13:09:51 +0800

    删除调试相关代码

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -6,7 +6,6 @@ from . import models
 
 def index(request):
     article = models.Article.objects.get(id=1)
-    print(article.content)
     return render(request,'blog/index.html',{'article':article})
 
 # Create your views here.
